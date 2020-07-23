a7c65c9d712c578e85bea4a3970baacea298e494
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2020-07-23 11:50:06 +0700

    added package_files function

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
+import os
 
 from setuptools import setup, find_packages
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
 setup(
     name='metal-test',
-    version='0.1.1',
+    version='0.1.2',
     description='A C/C++ toolset for bare metal & embedded developments',
     author='Klemens Morgenstern',
+    author_email='klemens@metal.ci',
     url='http://pypi.python.org/pypi/metal_test',
-    packages=['metal'],
+    packages=['metal', 'metal.gdb', 'metal.serial'],
+    package_data={'metal': package_files('src') + package_files('include')},
     install_requires=['argparse', 'pcpp', 'pyelftools', 'cxxfilt'],
     entry_points={
         'console_scripts': ['metal-serial-generate=metal.serial.generate:main',
