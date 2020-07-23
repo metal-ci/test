3ceeee7ed577298e9554ea3d78fdbbfdde33731d
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2020-07-23 09:30:34 +0700

    added flags module

---
+++ metal/flags.py
@@ -0,0 +1,27 @@
+import pathlib
+import sys
+
+
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-P', '--path',         help="Print the root path of the project",  action='store_true')
+parser.add_argument('-I', '--include-path', help="Print the include path for the compiler",  action='store_true')
+parser.add_argument('-S', '--source',       help="Source files for a backend", choices=['gdb', 'serial'])
+parser.add_argument('-N', '--newlib',       help='The defined for the different newlib modes available for serial', choices=['blocked', 'unchecked', 'full'])
+
+
+args = parser.parse_args()
+
+if args.path:
+    sys.stdout.write(str(pathlib.Path(__file__).parent.parent.absolute()) + ' ')
+if args.include_path:
+    sys.stdout.write('-I' + str(pathlib.Path(__file__).parent.parent.absolute() / 'include') + ' ')
+if args.source:
+    sys.stdout.write(str(pathlib.Path(__file__).parent.parent.absolute() / 'src' / 'metal' / args.source / 'syscalls.c' ) + ' ')
+if args.newlib == 'blocked':
+    sys.stdout.write('-DMETAL_SERIAL_SYSCALLS_MODE=METAL_SERIAL_SYSCALLS_MODE_BLOCKED ')
+elif args.newlib == 'unchecked':
+    sys.stdout.write('-DMETAL_SERIAL_SYSCALLS_MODE=METAL_SERIAL_SYSCALLS_MODE_UNCHECKED ')
+elif args.newlib == 'full':
+    sys.stdout.write('-DMETAL_SERIAL_SYSCALLS_MODE=METAL_SERIAL_SYSCALLS_MODE_FULL ')
